c805267c957b52131358a374563055c0d28e1bf0
Bill Napier <napier@google.com>
2023-04-13 01:03:31 +0000

    Add Create Organization call.

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,24 +1,65 @@
-from github_nonpublic_api import api
+"""Unit tests for api.py."""
 
 import os
+from unittest import TestCase, mock
+
 from truth.truth import AssertThat
-import unittest
-from unittest import mock
+
+from github_nonpublic_api import api
 
 GITHUB_FORM_HTML = os.path.join(os.path.dirname(__file__), 'github_form.html')
+NEW_ORG_FORM_HTML = os.path.join(
+    os.path.dirname(__file__), 'new_org_form.html')
 
 
-class TestApi(unittest.TestCase):
+class TestApi(TestCase):
+    def _seed_session_with_file(self, filename):
+        self.session = mock.MagicMock()
+        with open(filename) as fp:
+            self.session.get.return_value.text = fp.read()
+
+    """Unit tests for api.py."""
+
     def test_get_and_submit_form(self):
-        session = mock.MagicMock()
-        with open(GITHUB_FORM_HTML) as fp:
-            session.get.return_value.text = fp.read()
+        self._seed_session_with_file(GITHUB_FORM_HTML)
 
-        def DataCallback(data):
+        def _data_callback(data):
             data['add'] = 'yes'
 
         api._get_and_submit_form(
-            session=session, url='http://github.com', data_callback=DataCallback)
+            session=self.session, url='http://github.com', data_callback=_data_callback)
 
-        AssertThat(session.post).WasCalled().Once().With(
+        AssertThat(self.session.post).WasCalled().Once().With(
             'http://github.com/foo', data=dict(add='yes', key='value'))
+
+    def test_get_and_submit_form_by_id(self):
+        self._seed_session_with_file(GITHUB_FORM_HTML)
+
+        api._get_and_submit_form(
+            session=self.session, url='http://github.com', form_id='form2')
+
+        AssertThat(self.session.post).WasCalled().Once().With(
+            'http://github.com/form2', data=dict(key='value2'))
+
+    def test_get_and_submit_form_by_id_error(self):
+        self._seed_session_with_file(GITHUB_FORM_HTML)
+
+        with AssertThat(ValueError).IsRaised():
+            api._get_and_submit_form(
+                session=self.session, url='http://github.com', form_id='no_form')
+
+    def test_create_business_org(self):
+        self._seed_session_with_file(NEW_ORG_FORM_HTML)
+        gh = api.Api(session=self.session)
+        gh.create_organization(org_name='test', contact_email='nobody@google.com',
+                                org_usage=api.OrganizationUsage.BUSINESS, 
+                                business_name='A Fake Business')
+        AssertThat(self.session.post).WasCalled().Once().With(
+            'https://github.com/account/organizations/new_org', data={
+                'authenticity_token': 'value',
+                'agreed_to_terms': 'yes',
+                'terms_of_service_type': 'corporate',
+                'organization[billing_email]': 'nobody@google.com',
+                'organization[profile_name]': 'test',
+                'organization[company_name]': 'A Fake Business',
+            })
